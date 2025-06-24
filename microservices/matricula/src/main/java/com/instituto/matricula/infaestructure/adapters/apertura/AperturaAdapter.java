8b1dd3040ee45ffaa629e3bdc117259106ff7f62
Manuel60000 <142287832+Manuel60000@users.noreply.github.com>
2025-06-24 03:04:43 -0500

    Add files via upload

---
+++ microservices/matricula/src/main/java/com/instituto/matricula/infaestructure/adapters/apertura/AperturaAdapter.java
@@ -0,0 +1,40 @@
+package com.instituto.matricula.infaestructure.adapters.apertura;
+
+import com.instituto.matricula.domain.model.apertura.Apertura;
+import com.instituto.matricula.domain.model.apertura.AperturaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AperturaAdapter implements AperturaRepository {
+    private final AperturaDataRepository aperturaDataRepository;
+
+    public AperturaAdapter(AperturaDataRepository aperturaDataRepository) {
+        this.aperturaDataRepository = aperturaDataRepository;
+    }
+
+    @Override
+    public List<Apertura> findAll() {
+        return aperturaDataRepository.findAll();
+                .stream()
+                .map(AperturaMapper.MAPPER::toModel)
+                .toList();
+
+    }
+
+    @Override
+    public Apertura findById(String uid) {
+        return null;
+    }
+
+    @Override
+    public Apertura findByProfesorAndCurso(String uidProfesor, String uidCurso) {
+        return null;
+    }
+
+    @Override
+    public Apertura save(Apertura apertura) {
+        return null;
+    }
+}
---
+++ microservices/profesor/src/main/java/com/idat/profesor/infraestructure/entries/ProfesorController.java
@@ -0,0 +1,47 @@
+package com.idat.profesor.infraestructure.entries;
+
+import com.idat.profesor.domain.model.Profesor;
+import com.idat.profesor.domain.service.ProfesorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/instituto/profesor/")
+public class ProfesorController {
+
+    private final ProfesorService profesorService;
+
+    public ProfesorController(ProfesorService profesorService) {
+        this.profesorService = profesorService;
+    }
+
+    @GetMapping
+    ResponseEntity<List<Profesor>> findAll() {
+        return ResponseEntity.ok(profesorService.findAll());
+    }
+
+    @GetMapping("/{uid}")
+    ResponseEntity<Profesor> findByUid(@PathVariable("uid") String uid) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(profesorService.findById(uid));
+    }
+
+
+
+    @PutMapping("/{uid}")
+    public ResponseEntity<Profesor> updateProfesor(@PathVariable("uid") String uid, @RequestBody Profesor profesor){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(profesorService.update(uid, profesor));
+    }
+
+
+
+}
